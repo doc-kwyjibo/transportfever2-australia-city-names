968aaccb467e1a6d10be47dd1b95ee8ff5e2e2a9
doc kwyjibo <doc@kwyjibo.dev>
2022-02-21 12:01:40 +1000

    Create initial testing version

---
+++ mod.lua
@@ -0,0 +1,18 @@
+function data()
+return {
+  info = {
+    name = _("modTitle"),         
+    description = _("modDesc"),
+    authors = {
+      {
+        name = "doc_kwyjibo",
+		role = 'CREATOR',
+      },
+    },
+    minorVersion = 0,              
+    severityAdd = "NONE",       
+    severityRemove = "NONE",   
+    tags = { "map", "misc" }
+  },
+}
+end
---
+++ strings.lua
@@ -0,0 +1,8 @@
+function data()
+return {
+  en = {
+	modTitle = "Australian City Names",
+    modDesc = "Create cities with names of Australian cities.",
+  }
+}
+end
